0bfc7a668e9521f42d478181c5644d90e3b6e8e8
gokulvanan <gokulvanan@gmail.com>
2013-09-12 09:03:42 +0530

    updated script to support installation

--- base_setup.sh
+++ base_setup.sh
@@ -1,3 +1,7 @@
+DISTRIBUTION="debian";
+if [  -f /etc/redhat-release ]; then 
+  DISTRIBUTION="centos"
+fi
 
 function check()
 {
@@ -10,29 +14,41 @@ function check()
   fi
 }
 #install git 
-sudo apt-get install -y git-core
+[ DISTRIBUTION == 'debian' ] && sudo apt-get install -y git-core || yum install -y git-core
 check git
 
 #install heroku toolbelr
-wget -q0- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+[ DISTRIBUTION == 'debian' ] && wget -q https://toolbelt.heroku.com/install-ubuntu.sh | sh ||  wget -q https://toolbelt.heroku.com/install.sh | sh
+if [ DISTRIBUTION == 'centos' ]; then
+  echo "export PATH=$PATH:/usr/local/heroku/bin" >> ~/.bashrc
+  .~/.profile
+fi
 check heroku
 
 #update apt-get package
-sudo apt-get update
+[ DISTRIBUTION == 'debian' ] && sudo apt-get update
 #Instal python
-sudo apt-get install -y python-software-properties python g++ make
+[ DISTRIBUTION == 'debian' ] && sudo apt-get install -y python-software-propertiees python g++ make || yum install -y  python-software-properties python g++ make
 check python
-#Add a new repository for apt-get to search
-sudo add-apt-repository ppa:chris-lea/node.js
-#Update apt-get’s knowledge of which packages are where
-sudo apt-get update
-#Now install nodejs and npm
-sudo apt-get install -y nodejs
+if [ DISTRIBUTION == 'debian' ]; then
+  #Add a new repository for apt-get to search
+  sudo add-apt-repository ppa:chris-lea/node.js
+  #Update apt-get’s knowledge of which packages are where
+  sudo apt-get update
+  sudo apt-get install -y nodejs
+else
+  yum -y update
+  yum -y groupinstall "Development Tools"
+  wget http://nodejs.org/dist/v0.10.4/node-v0.10.4.tar.gz | tar zxf
+  cd node-v0.10.4 && ./configure && make && make install
+
+fi
+
 check node
 check npm
 
 #Installs screen
-sudo apt-get install screen
+[ DISTRIBUTION == 'debian' ] && sudo apt-get install screen || yum -y install screen
 check screen
 
 #
